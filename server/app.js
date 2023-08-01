0422a9be432f6a3ec4bcfd36b52ed2cb9b22a090
or <elhararor207@gmail.com>
2023-08-01 10:19:10 +0300

    Added env variables demo and server dummy project

---
+++ server/app.js
@@ -0,0 +1,12 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+
+import eventRouter from './routes/events.js';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(eventRouter);
+
+app.listen(process.env.PORT);
