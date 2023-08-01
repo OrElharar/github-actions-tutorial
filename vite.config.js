e3041fbf17e68f663f9468b100d6d255d50cf76e
or <elhararor207@gmail.com>
2023-08-01 13:12:05 +0300

    Fixed vitest config file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    exclude: ['**/node_modules/**', '**/dist/**', './server/**'],
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
