09ee2665e72e7aa7baef08938677886e21db3620
or <elhararor207@gmail.com>
2023-08-03 13:16:10 +0300

    Edited js custom action

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -14,14 +14,14 @@ const exec = require('@actions/exec');
 function run(){
     core.notice('Running deploy-s3-js action');
 
-    const bucket = core.getInput('bucket', {required: true});
-    const region = core.getInput('region', {required: true});
-    const distFolder = core.getInput('dist-folder', {required: true});
-
-    const s3Uri = `s3://${bucket}`;
-    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`)
-    const websiteUrl = `http://${bucket}.s3-website-${region}.amazonaws.com`;
-    core.setOutput('website-url', websiteUrl);
+    // const bucket = core.getInput('bucket', {required: true});
+    // const region = core.getInput('region', {required: true});
+    // const distFolder = core.getInput('dist-folder', {required: true});
+    //
+    // const s3Uri = `s3://${bucket}`;
+    // exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`)
+    // const websiteUrl = `http://${bucket}.s3-website-${region}.amazonaws.com`;
+    // core.setOutput('website-url', websiteUrl);
 }
 
 run();
