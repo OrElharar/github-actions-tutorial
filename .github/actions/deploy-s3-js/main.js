8b3a9ed75a1f37cd56b26af9d86aac60fb22bdc5
or <elhararor207@gmail.com>
2023-08-06 15:23:47 +0300

    Pushed JS action

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,15 +1,7 @@
-// 1. Get some input values
-// 2. Upload the files - while doing that, the aws cli would be searching for
-//    the following credentials in the environment variables: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
-// 3. Output the website url
+
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-// const github = require('@actions/github');
-// @actions/github would be useful to get access for:
-// 1. github.context  - context of the action - providing some useful information
-//    about the event like the type of the event, the payload, the repo, etc.
-// 2. github.getOctokit - get authenticated GitHub client - a tool to interact with
-//    the GitHub API easily
+
 
 function run(){
     core.notice('Running deploy-s3-js action');
